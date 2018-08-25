7ffa67851af5c6e352db6e256eec147eba02a942
René Abreu <rene.abreu@outlook.com>
2018-08-25 19:43:14 -0300

    Configuring SampleScene

---
+++ Assets/UltimateSpawner2D/Samples/Scripts/Square.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(AutoDeactivate());
+	}
+	
+	IEnumerator AutoDeactivate() {
+		yield return new WaitForSeconds(.5f);
+		gameObject.SetActive(false);
+	}
+}
