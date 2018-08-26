549c65ab127be9afc873dc161015ff5607b8af88
René Abreu <rene.abreu@outlook.com>
2018-08-25 23:09:01 -0300

    SpawnMode.Fixed Time working

--- Assets/UltimateSpawner2D/UltimateSpawner2D.cs
+++ Assets/UltimateSpawner2D/UltimateSpawner2D.cs
@@ -8,13 +8,21 @@ namespace UltimateSpawner2D {
 
 		#region Spawner Basic Settings
 
-		[Tooltip("Choose a Object to spawn")] public GameObject objectToSpawn;
+		[Tooltip("Choose a Object to spawn")] 
+		public GameObject objectToSpawn;
+		[Tooltip("Should we use object pooling?")] 
+		public bool usePoolSystem;
+		
+		public SpawnMode spawnMode;
+
+		// Show/Hide Debug Logs (I hate spammed logs)
+		public bool ShowDebugMessages;
 
 		#endregion
 
 		#region Spawner Advanced Settings
 
-		public bool ShowDebugMessages;
+		public KeyCode inputKeyCode;
 
 		#endregion
 
@@ -36,15 +44,45 @@ namespace UltimateSpawner2D {
 
 		#endregion
 
+		#region Timer Setup
+
+		// TODO: Configure first spawn (Time or Trigger)
+		[Tooltip("Time since script activation to spawn first object")] 
+		public float firstSpawnTime;
+		[Tooltip("Time between spawns. (after first spawn)")] 
+		public float delayBetweenSpawns;
+		
+
+		#endregion
+
 		#region UnityCalls
 
 		void Awake() {
-			StartPool();
+			if(usePoolSystem)
+				StartPool();
+			
+			if (spawnMode == SpawnMode.FixedTime || spawnMode == SpawnMode.ProgressiveTime ||
+			    spawnMode == SpawnMode.RandomTime)
+				SetupTimer();
 		}
 
 		void Update() {
-			if (Input.GetKeyDown(KeyCode.Space))
-				Spawn();
+
+			if (spawnMode == SpawnMode.Input) {
+				if (Input.GetKeyDown(inputKeyCode))
+					Spawn();
+			}
+
+			if (spawnMode == SpawnMode.FixedTime || spawnMode == SpawnMode.ProgressiveTime ||
+			    spawnMode == SpawnMode.RandomTime) {
+				
+				timeLeft -= Time.deltaTime;
+				if (timeLeft < 0) {
+					timeLeft = interval;
+					Elapsed.Invoke();
+				}
+			}
+			
 		}
 
 		#endregion
@@ -105,13 +143,59 @@ namespace UltimateSpawner2D {
 		#region Spawn
 
 		public void Spawn() {
-			
-			// Get Object in Pool
-			currentPoolGameObject = GetNextObject();
-			
-			// Activate it
-			if(currentPoolGameObject != null)
-				currentPoolGameObject.SetActive(true);
+
+			// Activate Pool Object
+			if (usePoolSystem) {
+				// Get Object in Pool
+				currentPoolGameObject = GetNextObject();
+
+				// Activate it
+				if (currentPoolGameObject != null)
+					currentPoolGameObject.SetActive(true);
+
+			}
+			// Instantiate New Object
+			else {
+				Instantiate(objectToSpawn, Vector2.zero, Quaternion.identity);
+			}
+		}
+		
+		#endregion
+
+		#region Timer
+		
+		private float interval;
+
+		private float timeLeft;
+
+		private delegate void TimerElapsed();
+
+		private event TimerElapsed Elapsed;
+
+		void SetupTimer() {
+			timeLeft = interval = firstSpawnTime;
+
+			Elapsed += Spawn;
+			Elapsed += ElapsedBehaviour;
+		}
+
+		void ElapsedBehaviour() {
+
+			switch (spawnMode) {
+					case SpawnMode.FixedTime:
+						interval = delayBetweenSpawns;
+						break;
+					case SpawnMode.ProgressiveTime:
+						// TODO: Calculate progressive time
+						break;
+					case SpawnMode.RandomTime:
+						// TODO: Get random time
+						break;
+					default:
+						if(Application.isEditor && ShowDebugMessages)
+							Debug.Log("Elapsed was called but timer is not configured");
+						break;
+			}
 		}
 		
 		#endregion
@@ -120,7 +204,9 @@ namespace UltimateSpawner2D {
 	public enum SpawnMode {
 		FixedTime,
 		ProgressiveTime,
-		RandomTime
+		RandomTime,
+		External,
+		Input
 	}
 	
 	
