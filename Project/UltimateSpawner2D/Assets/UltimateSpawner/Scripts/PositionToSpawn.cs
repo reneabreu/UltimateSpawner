487f417fa9531bd21c94a3e845c136b44fff7ab4
René Abreu <rene.abreu@outlook.com>
2018-08-26 10:50:24 -0300

    Fixing CreateAssetMenu from 'UltimateSpawner2D' to 'UltimateSpawner3D'

--- Project/UltimateSpawner2D/Assets/UltimateSpawner/Scripts/PositionToSpawn.cs
+++ Project/UltimateSpawner2D/Assets/UltimateSpawner/Scripts/PositionToSpawn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace UltimateSpawner {
 
-	[CreateAssetMenu(fileName = "Position To Spawn", menuName = "UltimateSpawner2D/Position To Spawn")]
+	[CreateAssetMenu(fileName = "Position To Spawn", menuName = "UltimateSpawner/Position To Spawn")]
 	public class PositionToSpawn : ScriptableObject {
 
 		public Vector3 vectorPosition;
